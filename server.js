67c5421a154ac0c890f2a99deeff7148ff27ee5c
iraklotses <134609606+iraklotses@users.noreply.github.com>
2025-02-12 19:43:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ CORS Middleware
 app.use(cors({
     origin: "https://gymsite-frontend.vercel.app", // Επιτρέπει το frontend URL
-    methods: "GET, POST, DELETE",
+    methods: "GET, POST, DELETE, PUT",
     allowedHeaders: "Content-Type, Authorization"
 }));
 
